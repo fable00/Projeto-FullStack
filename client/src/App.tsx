7d540c624cf76ba897d20dfa12bcb82fff1debe1
fable00 <saymonmcedo00@gmail.com>
2024-10-21 20:35:52 -0300

    Added React Router Dom, and set up routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,11 @@
+import { BrowserRouter } from "react-router-dom"
+import { AppRoutes } from "./routes"
+
 export const App = () => {
+ 
   return (
-    <>
-    <h1>Hello world!</h1>
-    </>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }
---
+++ client/src/routes/index.tsx
@@ -0,0 +1,10 @@
+import { Routes, Route, Navigate } from "react-router-dom"
+
+export const AppRoutes = () =>{
+    return(
+        <Routes>
+            <Route path="/home" element={<p>Pagina inicial</p>}></Route>
+            <Route path="*" element={<Navigate to={'/home'} />}></Route>
+        </Routes>
+    )
+}
