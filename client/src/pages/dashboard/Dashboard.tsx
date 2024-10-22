40afb3da3ba81b92c70e94943cbe4c029197964f
fable00 <saymonmcedo00@gmail.com>
2024-10-22 13:10:21 -0300

    Definied responsivity of base layout

--- client/src/pages/dashboard/Dashboard.tsx
+++ client/src/pages/dashboard/Dashboard.tsx
@@ -2,7 +2,7 @@ import { LayoutBaseDePagina } from '../../shared/layouts/'
 
 export const DashBoard = () => {
   return (
-    <LayoutBaseDePagina title={'Página Inicial'}>
+    <LayoutBaseDePagina title={'Página Inicial'} toolBar={<>Barra</>}>
       Teste.......
     </LayoutBaseDePagina>
   )
--- client/src/shared/layouts/LayoutBaseDePagina.tsx
+++ client/src/shared/layouts/LayoutBaseDePagina.tsx
@@ -11,21 +11,24 @@ import { useDrawerContext } from '../contexts'
 interface ILayoutBaseDePagina {
   children: ReactNode
   title: string
+  toolBar?: ReactNode
 }
 
 export const LayoutBaseDePagina: React.FC<ILayoutBaseDePagina> = ({
   children,
   title,
+  toolBar,
 }) => {
   const theme = useTheme()
   const smDown = useMediaQuery(theme.breakpoints.down('sm'))
+  const mdDown = useMediaQuery(theme.breakpoints.down('md'))
   const { toggleDrawerOpen } = useDrawerContext()
 
   return (
     <Box height={'100%'} display={'flex'} flexDirection={'column'} gap={1}>
       <Box
         padding={1}
-        height={theme.spacing(12)}
+        height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)}
         display={'flex'}
         alignItems={'center'}
         gap={1}
@@ -36,12 +39,21 @@ export const LayoutBaseDePagina: React.FC<ILayoutBaseDePagina> = ({
           </IconButton>
         )}
 
-        <Typography variant={'h5'}>{title}</Typography>
+        <Typography
+          variant={smDown ? 'h5' : mdDown ? 'h4' : 'h3'}
+          overflow={'hidden'}
+          whiteSpace={'nowrap'}
+          textOverflow={'ellipsis'}
+        >
+          {title}
+        </Typography>
       </Box>
 
-      <Box>Barra de ferramentas</Box>
+      {toolBar && <Box>{toolBar}</Box>}
 
-      <Box>{children}</Box>
+      <Box flex={1} overflow={'auto'}>
+        {children}
+      </Box>
     </Box>
   )
 }
